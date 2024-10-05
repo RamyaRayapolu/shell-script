387e8a8db07272dc470935607dedaefa2749c386
RamyaRayapolu <ramyarayapolu@gmail.com>
2024-10-05 18:27:08 +0530

    script

--- special-vars.sh
+++ special-vars.sh
@@ -11,3 +11,10 @@ echo "PID of last background command : $!"
 echo "check the status of previous cammnd: $?"
 
 echo "0 represents sucess and 1-127 represents failure"
+
+echo "First argument is $1"
+echo "Second argument is $2"
+sleep 50 & 
+echo "PID of last background command: $!"
+echo "Tells the state of previous command $?"
+# 0- success and anything 1-127 - failure
